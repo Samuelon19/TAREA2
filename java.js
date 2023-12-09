a01baa78062e24548a822f93d059784d014aa089
Samuelon19 <152840565+Samuelon19@users.noreply.github.com>
2023-12-08 23:12:00 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,111 @@
+//////////secciones
+
+        function showSection1(id) {
+            var secciones = document.querySelectorAll(".seccion1");
+            secciones.forEach(function(seccion) {
+                seccion.style.display = "none";
+            });
+
+            var seccion = document.getElementById(id);
+            seccion.style.display = "block";
+        }
+
+        function showSection2(id) {
+            var secciones = document.querySelectorAll(".seccion2");
+            secciones.forEach(function(seccion) {
+                seccion.style.display = "none";
+            });
+
+            var seccion = document.getElementById(id);
+            seccion.style.display = "block";
+        }
+
+        function showSection3(id) {
+            var secciones = document.querySelectorAll(".seccion3");
+            secciones.forEach(function(seccion) {
+                seccion.style.display = "none";
+            });
+
+            var seccion = document.getElementById(id);
+            seccion.style.display = "block";
+        }
+
+        function showSection4(id) {
+            var secciones = document.querySelectorAll(".seccion4");
+            secciones.forEach(function(seccion) {
+                seccion.style.display = "none";
+            });
+
+            var seccion = document.getElementById(id);
+            seccion.style.display = "block";
+        }
+
+        function showSection5(id) {
+            var secciones = document.querySelectorAll(".seccion5");
+            secciones.forEach(function(seccion) {
+                seccion.style.display = "none";
+            });
+
+            var seccion = document.getElementById(id);
+            seccion.style.display = "block";
+        }
+
+        function showSection6(id) {
+            var secciones = document.querySelectorAll(".seccion6");
+            secciones.forEach(function(seccion) {
+                seccion.style.display = "none";
+            });
+
+            var seccion = document.getElementById(id);
+            seccion.style.display = "block";
+        }
+
+        function showSection7(id) {
+            var secciones = document.querySelectorAll(".seccion7");
+            secciones.forEach(function(seccion) {
+                seccion.style.display = "none";
+            });
+
+            var seccion = document.getElementById(id);
+            seccion.style.display = "block";
+        }
+
+        function showSection8(id) {
+            var secciones = document.querySelectorAll(".seccion8");
+            secciones.forEach(function(seccion) {
+                seccion.style.display = "none";
+            });
+
+            var seccion = document.getElementById(id);
+            seccion.style.display = "block";
+        }
+
+        function showSection9(id) {
+            var secciones = document.querySelectorAll(".seccion9");
+            secciones.forEach(function(seccion) {
+                seccion.style.display = "none";
+            });
+
+            var seccion = document.getElementById(id);
+            seccion.style.display = "block";
+        }
+
+        function showSection10(id) {
+            var secciones = document.querySelectorAll(".seccion10");
+            secciones.forEach(function(seccion) {
+                seccion.style.display = "none";
+            });
+
+            var seccion = document.getElementById(id);
+            seccion.style.display = "block";
+        }
+
+        function showSection11(id) {
+            var secciones = document.querySelectorAll(".seccion11");
+            secciones.forEach(function(seccion) {
+                seccion.style.display = "none";
+            });
+
+            var seccion = document.getElementById(id);
+            seccion.style.display = "block";
+        }
